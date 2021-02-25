05830b56aeda8e81495143118c562f867299da3e
Mishal Zaman <mishalzaman+everlywell@gmail.com>
2021-02-25 08:26:16 -0800

    add home page and navigation

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
 
   get '/expert', to: 'expert#index'
 
-  
+  root :to => "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
